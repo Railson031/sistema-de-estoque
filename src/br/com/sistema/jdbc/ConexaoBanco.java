fc87de40e9a6bbaa279da58407b069114cb02c96
Railson Carvalho <railson031@hotmail.com>
2024-04-26 06:59:22 -0300

    refatorando o código e criando layout do sistema

---
+++ src/br/com/sistema/jdbc/ConexaoBanco.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.sistema.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author rails
+ */
+public class ConexaoBanco {
+    final private String url = "jdbc:mysql://localhost/sistemaestoque";
+    final private String usuario = "root";
+    final private String senha = "";
+    
+    public Connection pegarConexao(){
+        try {
+            return DriverManager.getConnection(url, usuario, senha);
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "erro ao se conectar com o banco de dados " + erro.getMessage());
+        }
+        
+        return null;
+    }
+    
+}
